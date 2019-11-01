3104ef5a92bb62e28baba60158fe173c91e6229a
victor1822 <victsales@gmail.com>
2019-11-01 12:11:08 -0300

    Acrescentando comentarios no arquivo Program.java

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,12 +11,14 @@ import entities.HourContract;
 import entities.Worker;
 import entities.enums.WorkerLevel;
 
-public class Program {
+public class Program { //programa principal 
 
 	public static void main(String[] args) throws ParseException {
-		Locale.setDefault(Locale.US);
+		Locale.setDefault(Locale.US); //utilizar o padrao internacional de numeros em ponto flutuante
 		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy"); //definir o formato de leitura da data para este padrao
+		
+		//Pegando os dados do colaborador e passando para a classe em si
 
 		System.out.print("Digite o nome do departamento: ");
 		String departmentName = sc.nextLine();
@@ -28,12 +30,14 @@ public class Program {
 		System.out.print("Salario-base: ");
 		double baseSalary = sc.nextDouble();
 
+		//passando o valor das variaveis para um objeto da classe Worker.
 		Worker worker = new Worker(workerName, WorkerLevel.valueOf(workerLevel), baseSalary,
-				new Department(departmentName));
-
+				new Department(departmentName)); //instanciando um novo objeto utilizando o construtor da classe Worker, o mesmo para a classe Department
 		System.out.print("Quantos contratos para este colaborador?");
+		//le o numero de contratos que se deseja digitar
 		int n = sc.nextInt();
-
+		
+		//passa os dados de cada contrato para a classe principal
 		for (int i = 0; i < n; i++) {
 			System.out.println("Digite os dados do contrato #" + i + ": ");
 			System.out.print("Date (DD/MM/YYYY): ");
@@ -46,6 +50,7 @@ public class Program {
 			worker.addContract(contract);
 		}
 
+		//printa na tela o resultado: Quanto o colaborador ganha, no mes que deu de entrada, de acordo com os contratos
 		System.out.print("Digite mes e ano para calcular o salario do mes (MM/YYYY): ");
 		String monthAndYear = sc.next();
 		int month = Integer.parseInt(monthAndYear.substring(0, 2));
